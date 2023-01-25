7c55da3ac6bfbd5015186a84e2c312857889ef49
msonowal <manash149@gmail.com>
2023-01-25 22:02:43 +0530

    updated with best practises

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <script setup>
-const { someData, branches} = await $fetch('/api/hello')
+// const { someData, branches} = await $fetch('/api/hello')
 
-console.log('log called ', someData, branches)
+const { data } = await useFetch('/api/hello', { pick: ['someData', 'branches'] })
+//we should use this as preferred and use the keys as BEST practices as below
+// https://nuxt.com/docs/getting-started/data-fetching#best-practices
+
+console.log('index called ', data.value.someData, ' branches', data.value.branches)
+
 </script>
 
 <template>
     <div>
-        Hello you are index.vue
+        Hello you are index.vue {{ data.someData }}
         <ul >
-            <li v-for="branch in branches">
+            <li v-for="branch in data.branches">
                 {{ branch.name }}
                 <pre>
                     {{ branch }}
--- server/api/hello.get.ts
+++ server/api/hello.get.ts
@@ -2,8 +2,11 @@
 const apiBase = useRuntimeConfig().apiBase
 export default defineEventHandler(async (event) => {
     // simple TEST sample
-    console.log('INTERNAL SERVER/API/HELLO called with ', 'current base url used'+ apiBase)
+    console.log('INTERNAL SERVER/API/HELLO called with current base url used '+ apiBase)
+    
     const { data } = await (await fetch(`${apiBase}branches`)).json()
+
+    console.log('data returning ', data)
 
     return {
         someData: 'works',
